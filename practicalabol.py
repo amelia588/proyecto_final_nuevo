631b3a8d442d6b6da81f71eabfb3a7063793ce70
Amelia Martel <1473967@senati.pe>
2023-02-28 13:57:55 -0500

    usamos valores panx, pady y guardamos cambios

--- practicalabol.py
+++ practicalabol.py
@@ -6,27 +6,27 @@ miFrame=Frame(width=1200,height=600)
 miFrame.pack()
 
 cuadroDNI=Entry(miFrame)
-cuadroDNI.grid(row=0, column=1)
+cuadroDNI.grid(row=0, column=1, padx=10, pady=10)
 
 cuadroApellido=Entry(miFrame)
-cuadroApellido.grid(row=1, column=1)
+cuadroApellido.grid(row=1, column=1, padx=10, pady=10)
 
 cuadroDirección=Entry(miFrame)
-cuadroDirección.grid(row=2, column=1)
+cuadroDirección.grid(row=2, column=1, padx=10, pady=10)
 
 cuadroTeléfono=Entry(miFrame)
-cuadroTeléfono.grid(row=3, column=1)
+cuadroTeléfono.grid(row=3, column=1, padx=10, pady=10)
 
 DNILabel=Label(miFrame, text="DNI:")
-DNILabel.grid(row=0, column=0)
+DNILabel.grid(row=0, column=0, sticky="w", padx=10, pady=10)
 
 ApellidoLabel=Label(miFrame, text="Apellido:")
-ApellidoLabel.grid(row=1, column=0)
+ApellidoLabel.grid(row=1, column=0, sticky="w", padx=10, pady=10)
 
 DirecciónLabel=Label(miFrame, text="Dirección:")
-DirecciónLabel.grid(row=2, column=0)
+DirecciónLabel.grid(row=2, column=0, sticky="w", padx=10, pady=10)
 
 TeléfonoLabel=Label(miFrame, text="Teléfono:")
-TeléfonoLabel.grid(row=3, column=0)
+TeléfonoLabel.grid(row=3, column=0, sticky="w", padx=10, pady=10)
 
 root.mainloop()
