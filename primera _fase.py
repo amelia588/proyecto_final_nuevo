ccb41d722112cb4d8cc6f585bc8a2f77a139d896
Amelia Martel <1473967@senati.pe>
2023-02-28 08:59:58 -0500

    cambiamos el color de freme

--- primera _fase.py
+++ primera _fase.py
@@ -6,12 +6,20 @@ raiz=Tk()
 
 raiz.title("ventana de pruebas")
 
-raiz.resizable(True,False)
+#raiz.resizable(True,False)
 
 raiz.iconbitmap("C:\proyecto_4\hrian.ico")
 
-raiz.geometry("650x350")
+#raiz.geometry("650x350")
 
 raiz.config(bg="pink")
+
+miFrame=Frame()
+
+miFrame.pack()
+
+miFrame.config(bg="yellow")
+
+miFrame.config(width="650",height="350")
 
 raiz.mainloop()
