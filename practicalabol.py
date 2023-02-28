cbef2b096805c1bd515c78d2edacc296bdcebd78
Amelia Martel <1473967@senati.pe>
2023-02-28 11:28:18 -0500

    asignamos valores a row y column para ordenar grid

--- practicalabol.py
+++ practicalabol.py
@@ -5,10 +5,28 @@ root=Tk()
 miFrame=Frame(width=1200,height=600)
 miFrame.pack()
 
-cuadrotexto=Entry(miFrame)
-cuadrotexto.place(x=100, y=100)
+cuadroDNI=Entry(miFrame)
+cuadroDNI.grid(row=0, column=1)
 
-DNILabel=Label(miFrame, text="DNI")
-DNILabel.place(x=100 , y=100)
+cuadroApellido=Entry(miFrame)
+cuadroApellido.grid(row=1, column=1)
+
+cuadroDirección=Entry(miFrame)
+cuadroDirección.grid(row=2, column=1)
+
+cuadroTeléfono=Entry(miFrame)
+cuadroTeléfono.grid(row=3, column=1)
+
+DNILabel=Label(miFrame, text="DNI:")
+DNILabel.grid(row=0, column=0)
+
+ApellidoLabel=Label(miFrame, text="Apellido:")
+ApellidoLabel.grid(row=1, column=0)
+
+DirecciónLabel=Label(miFrame, text="Dirección:")
+DirecciónLabel.grid(row=2, column=0)
+
+TeléfonoLabel=Label(miFrame, text="Teléfono:")
+TeléfonoLabel.grid(row=3, column=0)
 
 root.mainloop()
