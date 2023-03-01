f7cc51641e12042404d3fa668ed9697e47f47aea
Amelia Martel <1473967@senati.pe>
2023-02-28 23:23:05 -0500

    acabo de terminar el registro de datos del cliente

--- practicalabol.py
+++ practicalabol.py
@@ -114,11 +114,14 @@ Subtotal.grid(row=6, column=5, padx=10, pady=10)
 Subtotal=Entry(miFrame, width=7, state="readonly")
 Subtotal.grid(row=7, column=5, padx=10, pady=10)
 
+TotalLabel=Label(miFrame,text="Total")
+TotalLabel.grid(row=7, column=6, sticky="ew", padx=10, pady=10)
 
+Total=Entry(miFrame, width=7, state="readonly")
+Total.grid(row=7, column=7, padx=10, pady=10)
 
-
-
-
+Guardar=Button(miFrame, text="Guardar")
+Guardar.grid(row=8, column=2, padx=10, pady=10)
 
 
 
